0cf8d9953a80458a41c1e2f2fc25a0716ab8e478
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-05-21 17:25:14 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    const hq = 42
+    if (location >= hq) {
+        return (location - hq)
+    }
+    else if (location < hq) {
+        return (hq - location)
+    }
+}
+function distanceFromHqInFeet(location) {
+   const distFeet = (distanceFromHqInBlocks(location) * 264)
+   return (distFeet)
+}
+function distanceTravelledInFeet(start, stop) {
+   const feetTravelled = (Math.abs(stop - start) * 264)
+   return (feetTravelled)
+}
+
+function calculatesFarePrice(start, stop) {
+    const feetTravelled = (Math.abs(stop - start) * 264)
+    if (feetTravelled < 400) {
+        return 0
+    } else if (feetTravelled >= 400 && feetTravelled <= 2000) {
+        return ((feetTravelled - 400) * .02)
+    } else if (feetTravelled > 2000 && feetTravelled <= 2500) {
+        return 25
+    } else if ( feetTravelled > 2500) {
+        return 'cannot travel that far'
+    }
+}
